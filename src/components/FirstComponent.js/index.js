da5f2703b3b1a5be3d6cd61fd7096467f33996e2
fuatguzel <fuatguzel754@gmail.com>
2021-09-05 01:25:36 +0300

    Add module css

---
+++ src/components/FirstComponent.js/index.js
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+function FirstComponent() {
+  return (
+    <div className={styles.paragraph}>
+      <p>Hi</p>
+    </div>
+  )
+}
+
+export default FirstComponent
---
+++ src/components/SecondComponent.js/index.js
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+function SecondComponent() {
+  return (
+    <div className={styles.paragraph}>
+      <p>Hello</p>
+    </div>
+  )
+}
+
+export default SecondComponent
